987dd134aef3949eeb1b607f15bd2da7afd8b5af
ekimlin <emmakimlin@gmail.com>
2016-12-03 22:31:27 -0500

    Update TwoByThree.java

--- src/TwoByThree.java
+++ src/TwoByThree.java
@@ -7,6 +7,10 @@ public class TwoByThree {
 
     static ArrayList<Character> abcs = new ArrayList<Character>();
     static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    /* combos is a binary representation of the possible combinations of 2 letters where each letter
+     * appears three times. For examples, the combination of xxxyyy or aaabbb is represented in 
+     * binary by 000111. 
+    */
     static String[] combos = {"000111", "001011", "001101", "001110", "010011",
             "010101", "010110", "011001", "011010", "011100",
             "100011", "100101", "100110", "101001", "101010",
@@ -15,34 +19,38 @@ public class TwoByThree {
 
     public static void main(String[] args) {
 
-        //get input from command line
+        //get input from command line:
         String puzzle = args[0];
         globalInputString = puzzle;
-
-        //erase letters from the alphabet array
+        //fill abcs with any letter that is not in the user puzzle:
         prepareAlphabet(puzzle);
-
         //iterate through remaining alphabet letter combinations
-        //plug in combinations of letters for each combination
+        //plug in combinations of letters
         iterateLetterCombos();
     }
 
     static void iterateLetterCombos() {
 
-        for (int i = 0; i < abcs.size(); i++) {
-            for (int j = 0; j < abcs.size(); j++) {
+        for (int i = 0; i < abcs.size(); i++) { 
+            for (int j = 0; j < abcs.size(); j++) { 
                 if (i != j) {
                     tryCombinations(abcs.get(i), abcs.get(j));
                 }
             }
         }
     }
-
+    /* 
+     * Precondition: s is the puzzle the user provided as input, including underscores. 
+     * Precondition: abcs is empty 
+     * Postcondition: abcs is filled with all letters of the alphabet that do not appear
+     *                  in the user puzzle. 
+     */
     static void prepareAlphabet(String s) {
+        //Fill abcs with every letter of the alphabet:
         for (char c : alphabet.toCharArray()) {
             abcs.add(c);
         }
-
+        //Remove from abcs any letter that is present in the puzzle:
         System.out.println("Preparing the alphabet...");
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) != '_') {
@@ -53,7 +61,12 @@ public class TwoByThree {
             }
         }
     }
-
+    /*
+     * Try all of the possible combinations of a and b where a and b appear 3 times each. 
+     * For each combination, plug into the puzzle by replacing the underscores with a 
+     * letter. For example, if current combo is 'aaabbb' and the puzzle is "x_x_x_x_x_x_", 
+     * the result after replaceUnderscores is "xaxaxaxbxbxb"
+    */
     static void tryCombinations(char a, char b) {
         for (int i = 0; i < combos.length; i++) {
             char[] currentCombo = globalInputString.toCharArray();
@@ -61,7 +74,9 @@ public class TwoByThree {
 
         }
     }
-
+    /*
+     * Precondition: the number of underscores = the length of currentBinary = 6
+    */
     static void replaceUnderscores(char a, char b, char[] array, String currentBinary) {
         int counter = 0;
         for (int i = 0; i < array.length; i++) {
@@ -73,10 +88,7 @@ public class TwoByThree {
                 }
                 counter++;
             }
-
-
         }
-
-        System.out.println(String.valueOf(array));
+        System.out.println(String.valueOf(array)); //print the possible solution to the puzzle. 
     }
 }
